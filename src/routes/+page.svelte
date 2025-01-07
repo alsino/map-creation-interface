46efe35a2122bb038680b79db395ddb116799b59
alsino <alsino@users.noreply.github.com>
2025-01-07 11:16:18 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,23 @@
-<!-- src/routes/+page.svelte -->
 <script>
-	import ControlPanel from '$lib/components/ControlPanel.svelte';
-	import MapPanel from '$lib/components/MapPanel.svelte';
-	import DeployApp from '$lib/components/DeployApp.svelte';
+    import { browser } from '$app/environment';
+    import ControlPanel from '$lib/components/ControlPanel.svelte';
+    import MapPanel from '$lib/components/MapPanel.svelte';
+    import DeployApp from '$lib/components/DeployApp.svelte';
+
+    let isFullscreen = false;
+
+    $: if (browser) {
+        const params = new URLSearchParams(window.location.search);
+        isFullscreen = params.get('view') === 'fullscreen';
+    }
 </script>
 
 <div class="flex h-screen">
-	<div class="w-1/2 space-y-4 overflow-y-auto bg-gray-50 p-4">
-		<ControlPanel />
-		<DeployApp />
-	</div>
-	<MapPanel />
+    {#if !isFullscreen}
+        <div class="w-1/2 space-y-4 overflow-y-auto bg-gray-50 p-4">
+            <ControlPanel />
+            <DeployApp />
+        </div>
+    {/if}
+    <MapPanel class={isFullscreen ? 'w-full' : 'w-1/2'} />
 </div>
