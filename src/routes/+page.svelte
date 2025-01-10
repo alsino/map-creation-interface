c3bb1333039539373186e2db5d5fb6b0b637bbc5
alsino <iamahorse@gmail.com>
2025-01-10 12:18:40 +0100

    new height calculation-6

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,63 +1,22 @@
 <script>
 	import { page } from '$app/stores';
-	import { onMount } from 'svelte';
 	import ControlPanel from '$lib/components/ControlPanel.svelte';
 	import MapPanel from '$lib/components/MapPanel.svelte';
 	import DeployApp from '$lib/components/DeployApp.svelte';
 	import { APP_HEIGHT_NEW } from '$lib/stores/shared';
 
-	let containerRef;
-	let lastHeight = 0; // Track last height to prevent infinite updates
-
 	// Get the view parameter from the page store
 	$: isFullscreen = $page.url.searchParams.get('view') === 'fullscreen';
 
-	function calculateHeight() {
-		if (containerRef) {
-			const mapContainer = containerRef.querySelector('#euranet-map');
-			const sourceNotes = containerRef.querySelector('#source-notes');
-
-			const extraPadding = 40;
-			const totalHeight = (mapContainer?.scrollHeight || 0) + extraPadding;
-
-			// Only update if height has actually changed and is reasonable
-			if (totalHeight !== lastHeight && totalHeight > 0 && totalHeight < 5000) {
-				console.log('Height measurements:', {
-					mapContainer: mapContainer?.scrollHeight,
-					sourceNotes: sourceNotes?.offsetHeight,
-					totalHeight: totalHeight
-				});
-
-				lastHeight = totalHeight;
-				$APP_HEIGHT_NEW = totalHeight;
-				window.parent.postMessage({ height: totalHeight }, '*');
-			}
+	$: {
+		if ($APP_HEIGHT_NEW) {
+			console.log('$APP_HEIGHT_NEW', $APP_HEIGHT_NEW);
+			window.parent.postMessage({ height: $APP_HEIGHT_NEW }, '*');
 		}
 	}
-
-	onMount(() => {
-		// Initial calculation
-		setTimeout(calculateHeight, 500);
-
-		// Debounced resize observer to prevent rapid updates
-		let timeoutId;
-		const observer = new ResizeObserver(() => {
-			clearTimeout(timeoutId);
-			timeoutId = setTimeout(calculateHeight, 100);
-		});
-
-		if (containerRef) {
-			observer.observe(containerRef);
-		}
-
-		return () => {
-			observer.disconnect();
-			clearTimeout(timeoutId);
-		};
-	});
 </script>
 
-<div bind:this={containerRef} class="flex w-full">
+<div class="flex h-screen" bind:clientHeight={$APP_HEIGHT_NEW}>
 	{#if !isFullscreen}
 		<div class="w-1/2 space-y-4 overflow-y-auto bg-gray-50 p-4">
 			<ControlPanel />
